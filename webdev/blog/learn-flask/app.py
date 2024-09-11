4ac8dc763496dc71362a530428dab52a602a4489
Indu Shekhar Jha <126399927+indu-shekhar@users.noreply.github.com>
2024-09-11 10:03:58 +0000

    updating the syntax for abstraction and seprating greet and form

--- webdev/blog/learn-flask/app.py
+++ webdev/blog/learn-flask/app.py
@@ -4,4 +4,10 @@ app=(Flask(__name__))
 @app.route("/")
 def index():
     name=request.args.get("name", "World")
-    return render_template("index.html", placeholder=name)
+    return render_template("index.html", name=name)
+
+@app.route("/greet")
+def greet():
+    # name=request.args.get("name", "World")
+    # return render_template("greet.html", name=name)
+    return render_template("greet.html",name=request.args.get("name", "World")) 
