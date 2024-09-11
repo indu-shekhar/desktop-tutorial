557b61b227cd34caff52fa19e19523c65e2bb94f
Indu Shekhar Jha <126399927+indu-shekhar@users.noreply.github.com>
2024-09-11 11:23:37 +0000

    created a form submission web application initial submit

--- webdev/blog/learn-flask/app.py
+++ webdev/blog/learn-flask/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 app=(Flask(__name__))
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def index():
-    name=request.args.get("name", "World")
-    return render_template("index.html", name=name)
+    if request.method=="GET":
+        return render_template("index.html")
+    else:
+        return render_template("greet.html", name=request.form.get("name", "World"))
 
-@app.route("/greet")
-def greet():
-    # name=request.args.get("name", "World")
-    # return render_template("greet.html", name=name)
-    return render_template("greet.html",name=request.args.get("name", "World")) 
+# @app.route("/greet",methods=["POST"])
+# def greet():
+#     # name=request.args.get("name", "World")
+#     # return render_template("greet.html", name=name)
+#     return render_template("greet.html",name=request.form.get("name", "World")) 
