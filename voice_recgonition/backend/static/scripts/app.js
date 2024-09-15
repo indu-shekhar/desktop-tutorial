1e24303ce2befd26708d1e680f6de061b2c01647
Indu Shekhar Jha <126399927+indu-shekhar@users.noreply.github.com>
2024-09-15 17:05:37 +0000

    some chage in the audioplayer display

--- voice_recgonition/backend/static/scripts/app.js
+++ voice_recgonition/backend/static/scripts/app.js
@@ -5,7 +5,7 @@ const recordBtn = document.getElementById('record-btn');
 const stopBtn = document.getElementById('stop-btn');
 const audioPlayer = document.getElementById('player');
 const audioInput = document.getElementById('audio-file');
-audioPlayer.disabled = true;
+audioPlayer.style.display = 'none';
 
 recordBtn.addEventListener('click', async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -37,7 +37,7 @@ stopBtn.addEventListener('click', () => {
     mediaRecorder.stop();
     recordBtn.disabled = false;
     stopBtn.disabled = true;
-    audioPlayer.disabled = false;
+    audioPlayer.style.display = 'block';
 });
 
 
