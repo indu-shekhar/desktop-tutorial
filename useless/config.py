e83d5ed82a06572f3338a2eebb20952ff9285933
Indu Shekhar Jha <126399927+indu-shekhar@users.noreply.github.com>
2025-07-09 07:08:33 +0000

    discard unnecessry files

--- useless/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    print(SECRET_KEY)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(os.getcwd(), 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'alphabetagammadelta@epsilonzetaeta'
